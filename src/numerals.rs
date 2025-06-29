e21aea073f0634de64498e2f88e6b47283a2beed
Greg Unrein <greg@unre.in>
2025-06-29 00:44:43 -0700

    Major improvements to features, data structure, code, and tests

--- src/numerals.rs
+++ src/numerals.rs
@@ -1,3 +1,6 @@
+use crate::numerals::RomanNumeral::*;
+use std::fmt;
+use std::fmt::{Display, Formatter};
 /// Roman numerals based on the definitions at
 /// https://en.wikipedia.org/wiki/Roman_numerals
 use std::str::FromStr;
@@ -14,7 +17,7 @@ pub enum RomanNumeralError {
 
 pub type RomanNumeralResult = Result<RomanNumeral, RomanNumeralError>;
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Clone, Debug)]
 pub enum RomanNumeral {
     // Individual numerals
     I,
@@ -42,7 +45,7 @@ impl FromStr for RomanNumeral {
         let trimmed = s.trim();
         let (ok_numerals, errors): (Vec<RomanNumeralResult>, Vec<RomanNumeralResult>) =
             UnicodeSegmentation::graphemes(trimmed, true)
-                .map(|grapheme| parse_numeral_from_grapheme(grapheme))
+                .map(parse_numeral_from_grapheme)
                 .partition(Result::is_ok);
 
         if errors.is_empty() {
@@ -50,7 +53,6 @@ impl FromStr for RomanNumeral {
                 .into_iter()
                 // unwrap should be safe here since there are no error results in the vec based on the partition above
                 .map(|r| r.unwrap())
-                .into_iter()
                 .peekable();
 
             // Reduce any subtractive forms to their singular representations
@@ -58,45 +60,45 @@ impl FromStr for RomanNumeral {
             while let Some(numeral) = numerals_iter.next() {
                 if let Some(next) = numerals_iter.peek() {
                     match numeral {
-                        RomanNumeral::I => {
-                            if next == &RomanNumeral::V {
+                        I => {
+                            if next == &V {
                                 // Merge into a single numeral
-                                simplified_numerals.push(RomanNumeral::IV);
+                                simplified_numerals.push(IV);
                                 // and consume the second numeral
                                 numerals_iter.next();
-                            } else if next == &RomanNumeral::X {
+                            } else if next == &X {
                                 // Merge into a single numeral
-                                simplified_numerals.push(RomanNumeral::IX);
-                                // and consume the second numeral
-                                numerals_iter.next();
-                            } else {
-                                simplified_numerals.push(numeral);
-                            }
-                        }
-                        RomanNumeral::X => {
-                            if next == &RomanNumeral::L {
-                                // Merge into a single numeral
-                                simplified_numerals.push(RomanNumeral::XL);
-                                // and consume the second numeral
-                                numerals_iter.next();
-                            } else if next == &RomanNumeral::C {
-                                // Merge into a single numeral
-                                simplified_numerals.push(RomanNumeral::XC);
+                                simplified_numerals.push(IX);
                                 // and consume the second numeral
                                 numerals_iter.next();
                             } else {
                                 simplified_numerals.push(numeral);
                             }
                         }
-                        RomanNumeral::C => {
-                            if next == &RomanNumeral::D {
+                        X => {
+                            if next == &L {
                                 // Merge into a single numeral
-                                simplified_numerals.push(RomanNumeral::CD);
+                                simplified_numerals.push(XL);
                                 // and consume the second numeral
                                 numerals_iter.next();
-                            } else if next == &RomanNumeral::M {
+                            } else if next == &C {
                                 // Merge into a single numeral
-                                simplified_numerals.push(RomanNumeral::CM);
+                                simplified_numerals.push(XC);
+                                // and consume the second numeral
+                                numerals_iter.next();
+                            } else {
+                                simplified_numerals.push(numeral);
+                            }
+                        }
+                        C => {
+                            if next == &D {
+                                // Merge into a single numeral
+                                simplified_numerals.push(CD);
+                                // and consume the second numeral
+                                numerals_iter.next();
+                            } else if next == &M {
+                                // Merge into a single numeral
+                                simplified_numerals.push(CM);
                                 // and consume the second numeral
                                 numerals_iter.next();
                             } else {
@@ -104,17 +106,9 @@ impl FromStr for RomanNumeral {
                             }
                         }
                         // All of these cases simply add themselves
-                        RomanNumeral::V
-                        | RomanNumeral::L
-                        | RomanNumeral::D
-                        | RomanNumeral::M
-                        | RomanNumeral::IV
-                        | RomanNumeral::IX
-                        | RomanNumeral::XL
-                        | RomanNumeral::XC
-                        | RomanNumeral::CD
-                        | RomanNumeral::CM
-                        | RomanNumeral::Compound(_) => simplified_numerals.push(numeral),
+                        V | L | D | M | IV | IX | XL | XC | CD | CM | Compound(_) => {
+                            simplified_numerals.push(numeral)
+                        }
                     }
                 } else {
                     // No next numeral so just add the current one
@@ -129,7 +123,7 @@ impl FromStr for RomanNumeral {
                 Ok(simplified_numerals.pop().unwrap())
             } else {
                 // Must be a compound numeral
-                Ok(RomanNumeral::Compound(simplified_numerals))
+                Ok(Compound(simplified_numerals))
             }
         } else {
             let unwrapped_errors = errors.into_iter().map(|e| e.unwrap_err()).collect();
@@ -140,13 +134,13 @@ impl FromStr for RomanNumeral {
 
 fn parse_numeral_from_grapheme(grapheme: &str) -> RomanNumeralResult {
     match grapheme {
-        "I" => Ok(RomanNumeral::I),
-        "V" => Ok(RomanNumeral::V),
-        "X" => Ok(RomanNumeral::X),
-        "L" => Ok(RomanNumeral::L),
-        "C" => Ok(RomanNumeral::C),
-        "D" => Ok(RomanNumeral::D),
-        "M" => Ok(RomanNumeral::M),
+        "I" => Ok(I),
+        "V" => Ok(V),
+        "X" => Ok(X),
+        "L" => Ok(L),
+        "C" => Ok(C),
+        "D" => Ok(D),
+        "M" => Ok(M),
         other => Err(RomanNumeralError::NotRomanNumeral(other.to_string())),
     }
 }
@@ -165,136 +159,263 @@ impl From<RomanNumeral> for i32 {
 
 fn into_i32(numeral: &RomanNumeral) -> i32 {
     match numeral {
-        RomanNumeral::I => 1,
-        RomanNumeral::V => 5,
-        RomanNumeral::X => 10,
-        RomanNumeral::L => 50,
-        RomanNumeral::C => 100,
-        RomanNumeral::D => 500,
-        RomanNumeral::M => 1000,
-        RomanNumeral::IV => 4,
-        RomanNumeral::IX => 9,
-        RomanNumeral::XL => 40,
-        RomanNumeral::XC => 90,
-        RomanNumeral::CD => 400,
-        RomanNumeral::CM => 900,
-        RomanNumeral::Compound(numerals) => numerals
+        I => 1,
+        V => 5,
+        X => 10,
+        L => 50,
+        C => 100,
+        D => 500,
+        M => 1000,
+        IV => 4,
+        IX => 9,
+        XL => 40,
+        XC => 90,
+        CD => 400,
+        CM => 900,
+        Compound(numerals) => numerals
             .iter()
             .map(into_i32)
             .reduce(|left, right| left + right)
-            .map_or(0, |f| f),
+            .map_or(0, |i| i),
+    }
+}
+
+impl Display for RomanNumeral {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", into_string(self))
+    }
+}
+
+fn into_string(numeral: &RomanNumeral) -> String {
+    match numeral {
+        I => "I".into(),
+        V => "V".into(),
+        X => "X".into(),
+        L => "L".into(),
+        C => "C".into(),
+        D => "D".into(),
+        M => "M".into(),
+        IV => "IV".into(),
+        IX => "IX".into(),
+        XL => "XL".into(),
+        XC => "XC".into(),
+        CD => "CD".into(),
+        CM => "CM".into(),
+        Compound(numerals) => numerals
+            .iter()
+            .map(into_string)
+            .reduce(|left, right| format!("{left}{right}"))
+            .map_or("".into(), |s| s),
+    }
+}
+
+impl TryFrom<i32> for RomanNumeral {
+    type Error = RomanNumeralError;
+
+    fn try_from(value: i32) -> Result<Self, Self::Error> {
+        // Check bounds for standard form of Roman numerals
+        if !(1..=3999).contains(&value) {
+            Err(RomanNumeralError::NotRomanNumeral(value.to_string()))
+        } else {
+            let mut numeral = Vec::new();
+
+            let thousands = value / 1000;
+            for _ in 0..thousands {
+                numeral.push(M);
+            }
+
+            let hundreds = (value - (thousands * 1000)) / 100;
+            match hundreds {
+                0 => {} // no op
+                1 => numeral.push(C),
+                2 => numeral.push(Compound(vec![C, C])),
+                3 => numeral.push(Compound(vec![C, C, C])),
+                4 => numeral.push(CD),
+                5 => numeral.push(D),
+                6 => numeral.push(Compound(vec![D, C])),
+                7 => numeral.push(Compound(vec![D, C, C])),
+                8 => numeral.push(Compound(vec![D, C, C, C])),
+                9 => numeral.push(CM),
+                out_of_bounds => {
+                    return Err(RomanNumeralError::NotRomanNumeral(format!(
+                        "{value} has an invalid hundreds amount {out_of_bounds}"
+                    )))
+                }
+            }
+
+            let tens = (value - (thousands * 1000 + hundreds * 100)) / 10;
+            match tens {
+                0 => {} // no op
+                1 => numeral.push(X),
+                2 => numeral.push(Compound(vec![X, X])),
+                3 => numeral.push(Compound(vec![X, X, X])),
+                4 => numeral.push(XL),
+                5 => numeral.push(L),
+                6 => numeral.push(Compound(vec![L, X])),
+                7 => numeral.push(Compound(vec![L, X, X])),
+                8 => numeral.push(Compound(vec![L, X, X, X])),
+                9 => numeral.push(XC),
+                out_of_bounds => {
+                    return Err(RomanNumeralError::NotRomanNumeral(format!(
+                        "{value} has an invalid tens amount {out_of_bounds}"
+                    )))
+                }
+            }
+
+            let ones = value - (thousands * 1000 + hundreds * 100 + tens * 10);
+            match ones {
+                0 => {} // no op
+                1 => numeral.push(I),
+                2 => numeral.push(Compound(vec![I, I])),
+                3 => numeral.push(Compound(vec![I, I, I])),
+                4 => numeral.push(IV),
+                5 => numeral.push(V),
+                6 => numeral.push(Compound(vec![V, I])),
+                7 => numeral.push(Compound(vec![V, I, I])),
+                8 => numeral.push(Compound(vec![V, I, I, I])),
+                9 => numeral.push(IX),
+                out_of_bounds => {
+                    return Err(RomanNumeralError::NotRomanNumeral(format!(
+                        "{value} has an invalid tens amount {out_of_bounds}"
+                    )))
+                }
+            }
+
+            // Flatten any nested Compound numerals
+            let mut flattened: Vec<RomanNumeral> =
+                numeral.iter().flat_map(flatten_numeral).collect();
+
+            if flattened.is_empty() {
+                Err(RomanNumeralError::NotRomanNumeral(value.to_string()))
+            } else if flattened.len() == 1 {
+                // If there is a single numeral already, just return it
+                Ok(flattened.pop().unwrap())
+            } else {
+                // Must be a compound numeral
+                Ok(Compound(flattened))
+            }
+        }
+    }
+}
+
+fn flatten_numeral(numeral: &RomanNumeral) -> Vec<RomanNumeral> {
+    match numeral {
+        I | V | X | L | C | D | M | IV | IX | XL | XC | CD | CM => vec![numeral.clone()],
+        Compound(numerals) => numerals.iter().flat_map(flatten_numeral).collect(),
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::numerals::RomanNumeral::*;
+
+    fn check_numeral(input: &str, expected_numeral: RomanNumeral, expected_int: i32) {
+        let n = input.parse::<RomanNumeral>().unwrap();
+        assert_eq!(n, expected_numeral);
+        let i: i32 = n.into();
+        assert_eq!(i, expected_int);
+        let r: RomanNumeral = i.try_into().unwrap();
+        assert_eq!(r, expected_numeral);
+        assert_eq!(r.to_string(), input);
+    }
 
     #[test]
     fn one() {
-        let n = "I".parse::<RomanNumeral>().unwrap();
-        assert_eq!(n, I);
-        assert_eq!(i32::from(n), 1);
+        check_numeral("I", I, 1);
     }
 
     #[test]
     fn four() {
-        let n = "IV".parse::<RomanNumeral>().unwrap();
-        assert_eq!(n, IV);
-        assert_eq!(i32::from(n), 4);
+        check_numeral("IV", IV, 4);
     }
 
     #[test]
     fn five() {
-        let n = "V".parse::<RomanNumeral>().unwrap();
-        assert_eq!(n, RomanNumeral::V);
-        assert_eq!(i32::from(n), 5);
+        check_numeral("V", V, 5);
     }
 
     #[test]
     fn nine() {
-        let n = "IX".parse::<RomanNumeral>().unwrap();
-        assert_eq!(n, IX);
-        assert_eq!(i32::from(n), 9);
+        check_numeral("IX", IX, 9);
     }
 
     #[test]
     fn ten() {
-        let n = "X".parse::<RomanNumeral>().unwrap();
-        assert_eq!(n, RomanNumeral::X);
-        assert_eq!(i32::from(n), 10);
+        check_numeral("X", X, 10);
     }
 
     #[test]
     fn forty() {
-        let n = "XL".parse::<RomanNumeral>().unwrap();
-        assert_eq!(n, XL);
-        assert_eq!(i32::from(n), 40);
+        check_numeral("XL", XL, 40);
     }
 
     #[test]
     fn fifty() {
-        let n = "L".parse::<RomanNumeral>().unwrap();
-        assert_eq!(n, RomanNumeral::L);
-        assert_eq!(i32::from(n), 50);
+        check_numeral("L", L, 50);
     }
 
     #[test]
     fn ninety() {
-        let n = "XC".parse::<RomanNumeral>().unwrap();
-        assert_eq!(n, XC);
-        assert_eq!(i32::from(n), 90);
+        check_numeral("XC", XC, 90);
     }
 
     #[test]
     fn one_hundred() {
-        let n = "C".parse::<RomanNumeral>().unwrap();
-        assert_eq!(n, RomanNumeral::C);
-        assert_eq!(i32::from(n), 100);
+        check_numeral("C", C, 100);
     }
 
     #[test]
     fn four_hundred() {
-        let n = "CD".parse::<RomanNumeral>().unwrap();
-        assert_eq!(n, CD);
-        assert_eq!(i32::from(n), 400);
+        check_numeral("CD", CD, 400);
     }
 
     #[test]
     fn five_hundred() {
-        let n = "D".parse::<RomanNumeral>().unwrap();
-        assert_eq!(n, RomanNumeral::D);
-        assert_eq!(i32::from(n), 500);
+        check_numeral("D", D, 500);
     }
 
     #[test]
     fn nine_hundred() {
-        let n = "CM".parse::<RomanNumeral>().unwrap();
-        assert_eq!(n, CM);
-        assert_eq!(i32::from(n), 900);
+        check_numeral("CM", CM, 900);
     }
 
     #[test]
     fn one_thousand() {
-        let n = "M".parse::<RomanNumeral>().unwrap();
-        assert_eq!(n, RomanNumeral::M);
-        assert_eq!(i32::from(n), 1000);
+        check_numeral("M", M, 1000);
     }
 
     #[test]
     fn one_thousand_six_hundred_sixty_six() {
         // Tests all singular numerals in one pass
-        let n = "MDCLXVI".parse::<RomanNumeral>().unwrap();
-        assert_eq!(n, Compound(vec![M, D, C, L, X, V, I]));
-        assert_eq!(i32::from(n), 1666);
+        check_numeral("MDCLXVI", Compound(vec![M, D, C, L, X, V, I]), 1666);
     }
 
     #[test]
     fn largest_roman_numeral_in_standard_form() {
-        let n = "MMMCMXCIX".parse::<RomanNumeral>().unwrap();
-        assert_eq!(n, Compound(vec![M, M, M, CM, XC, IX]));
-        assert_eq!(i32::from(n), 3999);
+        check_numeral("MMMCMXCIX", Compound(vec![M, M, M, CM, XC, IX]), 3999);
+    }
+
+    #[test]
+    fn out_of_bounds() {
+        let result: RomanNumeralResult = 0.try_into();
+        let e = result.unwrap_err();
+        match e {
+            RomanNumeralError::NotRomanNumeral(value) if value == "0".to_string() => {} // correct
+            RomanNumeralError::NotRomanNumeral(value) => panic!("Incorrect value {value}"),
+            RomanNumeralError::NumeralsHasError(_) => {
+                panic!("Incorrect error type")
+            }
+        }
+
+        let result: RomanNumeralResult = 4000.try_into();
+        let e = result.unwrap_err();
+        match e {
+            RomanNumeralError::NotRomanNumeral(value) if value == "4000".to_string() => {} // correct
+            RomanNumeralError::NotRomanNumeral(value) => panic!("Incorrect value {value}"),
+            RomanNumeralError::NumeralsHasError(_) => {
+                panic!("Incorrect error type")
+            }
+        }
     }
 
     #[test]
@@ -302,25 +423,21 @@ mod tests {
         assert_eq!(i32::from(Compound(vec![Compound(vec![M, C, IX])])), 1109);
     }
 
+    fn check_empty_error(e: RomanNumeralError) {
+        match e {
+            RomanNumeralError::NotRomanNumeral(value) if value == "".to_string() => {} // correct
+            RomanNumeralError::NotRomanNumeral(value) => panic!("Incorrect value {value}"),
+            RomanNumeralError::NumeralsHasError(_) => {
+                panic!("Incorrect error type")
+            }
+        }
+    }
+
     #[test]
     fn parse_errors() {
-        let e = "".parse::<RomanNumeral>().unwrap_err();
-        match e {
-            RomanNumeralError::NotRomanNumeral(value) if value == "".to_string() => {} // correct
-            RomanNumeralError::NotRomanNumeral(value) => panic!("Incorrect value {value}"),
-            RomanNumeralError::NumeralsHasError(_) => {
-                panic!("Incorrect error type")
-            }
-        }
+        check_empty_error("".parse::<RomanNumeral>().unwrap_err());
 
-        let e = "     ".parse::<RomanNumeral>().unwrap_err();
-        match e {
-            RomanNumeralError::NotRomanNumeral(value) if value == "".to_string() => {} // correct
-            RomanNumeralError::NotRomanNumeral(value) => panic!("Incorrect value {value}"),
-            RomanNumeralError::NumeralsHasError(_) => {
-                panic!("Incorrect error type")
-            }
-        }
+        check_empty_error("     ".parse::<RomanNumeral>().unwrap_err());
 
         let e = " I Z V X L P C 🔺D M ".parse::<RomanNumeral>().unwrap_err();
         match e {
